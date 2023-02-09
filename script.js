cc2164803081900b709be517fa2305b5ac2949e4
tnyima19 <66368294+tnyima19@users.noreply.github.com>
2023-02-09 00:31:39 -0500

    created a button which shows an image in the home screen some times multiple attempts needed I also created a hover effect on teh links

---
+++ script.js
@@ -0,0 +1,19 @@
+"use strict";
+
+function showGraphImage(){
+    var img1 = document.getElementById('graph');
+    if(img1.style.visibility === 'hidden'){
+        img1.style.visibility = 'visible';
+    }else {
+        img1.style.visibility = 'hidden';
+    }
+}
+
+function showPicsImage(){
+    var img1 = document.getElementById('pics');
+    if(img1.style.visibility === 'hidden'){
+        img1.style.visibility = 'visible';
+    }else {
+        img1.style.visibility = 'hidden';
+    }
+}
