da67c6c4ee02e3d818ce41b3aedf1b36fa38886d
tnyima19 <66368294+tnyima19@users.noreply.github.com>
2023-02-10 18:58:49 -0500

    change background color after pressing button

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ function showGraphImage(){
     var img1 = document.getElementById('graph');
     if(img1.style.visibility === 'hidden'){
         img1.style.visibility = 'visible';
+       
     }else {
         img1.style.visibility = 'hidden';
     }
@@ -13,7 +14,9 @@ function showPicsImage(){
     var img1 = document.getElementById('pics');
     if(img1.style.visibility === 'hidden'){
         img1.style.visibility = 'visible';
+        document.body.style.backgroundColor = '#f1b335';
     }else {
         img1.style.visibility = 'hidden';
+        document.body.style.backgroundColor = '#f2eaf2';
     }
 }
